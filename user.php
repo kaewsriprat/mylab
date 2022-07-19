06d0c5b109ca191029ccb73abd5adca05a496964
Tong <kaewsriprat@gmail.com>
2022-07-19 15:15:22 +0700

    master

---
+++ user.php
@@ -0,0 +1,23 @@
+<?php
+
+function showTable()
+{
+    echo "<table class='table table-striped'>";
+    echo "<thead>";
+    echo "<tr>";
+    echo "<th>ID</th>";
+    echo "<th>Name</th>";
+    echo "<th>Email</th>";
+    echo "<th>Phone</th>";
+    echo "</tr>";
+    echo "</thead>";
+    echo "<tbody>";
+    echo "<tr>";
+    echo "<td> 1 </td>";
+    echo "<td> TEST </td>";
+    echo "<td> TEST@TEST </td>";
+    echo "<td> 123456  </td>";
+    echo "</tr>";
+    echo "</tbody>";
+    echo "</table>";
+}
